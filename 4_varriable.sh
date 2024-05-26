ee019af9b30dac0f2a1ff81ca6c8a98e6d9c184c
mayur2905 <mayurgaikwad0101@gmail.com>
2024-05-27 01:40:18 +0530

    Update variable.sh with corrected variable name and formatting

--- 4_varriable.sh
+++ 4_varriable.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 # User defined variables
 # variables are case sensitive
@@ -8,37 +8,37 @@
 # valid variable name: name, _name, name1
 # invalid variable name: 1name, na-me, na@me
 
-# name=Mayur
-# age=24
+name=Mayur
+age=24
 
-# echo $name
+echo $name
 
-# # using $VariableName
-# echo  "My $name is Mayur and my age is $age"
+# using $VariableName
+echo  "My  is ${name} and my age is $age"
 
-# # Curly braces
-# echo  "My ${name} is Mayur and my age is ${age}"
-# echo  'My ${name} is Mayur and my age is ${age}' # single quotes will not expand the variable
-# work=DevOps
-# post=Engineer
-# echo "I am a ${work} ${post} "
+# Curly braces
+echo  "My name is ${name} and my age is ${age}"
+echo  'My name is ${name} and my age is ${age}' # single quotes will not expand the variable
+work=DevOps
+post=Engineer
+echo "I am a ${work} ${post} "
 
-# Ststem defined variables
-# this variables are defined by the system and we can use them
-# HOME, PATH, PWD, SHELL, USER, LOGNAME, HOSTNAME, UID, GROUPS, etc
-# echo "My home directory is ${HOME}"
-# echo "My current working directory is ${PWD}"
-# echo "My shell is ${SHELL}"
-# echo "My user is ${USER}"
-# echo "My logname is ${LOGNAME}"
-# echo "My hostname is ${HOSTNAME}"
-# echo "My UID is ${UID}"
-# echo "My groups are ${GROUPS}"
-# echo "My path is ${PATH}"
-# echo "My current terminal is ${TERM}"
-# echo "My current editor is ${EDITOR}"
-# echo "My current display is ${DISPLAY}"
-# echo "My current language is ${LANG}"
-# echo "My current process id ${PPID}"
-# sleep 5
-# echo ${SECONDS}
+# System defined variables
+this variables are defined by the system and we can use them
+HOME, PATH, PWD, SHELL, USER, LOGNAME, HOSTNAME, UID, GROUPS, etc
+echo "My home directory is ${HOME}"
+echo "My current working directory is ${PWD}"
+echo "My shell is ${SHELL}"
+echo "My user is ${USER}"
+echo "My logname is ${LOGNAME}"
+echo "My hostname is ${HOSTNAME}"
+echo "My UID is ${UID}"
+echo "My groups are ${GROUPS}"
+echo "My path is ${PATH}"
+echo "My current terminal is ${TERM}"
+echo "My current editor is ${EDITOR}"
+echo "My current display is ${DISPLAY}"
+echo "My current language is ${LANG}"
+echo "My current process id ${PPID}"
+sleep 5
+echo ${SECONDS}
---
+++ 5_cmd_line_arg.sh
@@ -0,0 +1,30 @@
+# # Taking arguments from command line
+
+# #!/bin/bash
+
+# read name
+# read age
+
+# # using $VariableName
+# echo  "My name is ${name} and my age is ${age}"
+
+# # Taking arguments from command line and prompt the user for enter values
+# read -p "Please Enter your name " name
+# read -p "Please Enter Your age " age
+# read -p "Please Enter Yuor Password " -s password
+
+# # using $VariableName
+# echo  "My name is ${name} and my age is ${age}" \
+# Your password is ${password}
+
+
+#!/bin/bash
+
+echo ${0}
+echo ${1}
+echo ${2}
+
+name=${0}
+ages=${1}
+
+echo "My name is ${name} and my age is ${age}"
