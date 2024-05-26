87e960fd58f1097d493e8328910df5042a0be8cd
mayur2905 <mayurgaikwad0101@gmail.com>
2024-05-27 00:42:41 +0530

    chore: Update comment.sh with improved comments formatting

--- comment.sh
+++ comment.sh
@@ -2,28 +2,28 @@
 
 #!/bin/bash
 
-# # This is a comment
-# echo "Hello this is first shell script" # This is also a inline comment
+# This is a comment
+echo "Hello this is first shell script" # This is also a inline comment
 
-# # Strong quotes
-# echo ' my \
-# name \
-# is \
-# Mayur\
-# '
+# Strong quotes
+echo ' my \
+name \
+is \
+Mayur\
+'
 
-# # Double quotes
-# echo "  my \
-# name \
-# is \
-# Mayur\
-# " # This is a multi-line comment
+# Double quotes
+echo "  my \
+name \
+is \
+Mayur\
+" # This is a multi-line comment
 
-# # tab in line
-# echo -e "Hello\tWorld"
+# tab in line
+echo -e "Hello\tWorld"
 
-# # New line in line
-# echo -e "Hello\nWorld"
+# New line in line
+echo -e "Hello\nWorld"
 
-# # Backspace in line
+# Backspace in line
 echo -e "Hello\bWorld"
