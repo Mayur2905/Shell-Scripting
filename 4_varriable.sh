bbdf22ee7b4395bc4c87bccc3b54aab8ffedb266
mayur2905 <mayurgaikwad0101@gmail.com>
2024-05-27 00:59:37 +0530

    chore: Update variable.sh with corrected variable name and formatting

--- 4_varriable.sh
+++ 4_varriable.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
 # User defined variables
-name=Mayur
-age=24
+# variables are case sensitive
+# No spaces between variable name and value
+# No special characters in variable name
+# No numbers in the beginning of variable name
+# valid variable name: name, _name, name1
+# invalid variable name: 1name, na-me, na@me
+
+# name=Mayur
+# age=24
+
+# echo $name
+
+# # using $VariableName
+# echo  "My $name is Mayur and my age is $age"
+
+# # Curly braces
+# echo  "My ${name} is Mayur and my age is ${age}"
+# echo  'My ${name} is Mayur and my age is ${age}' # single quotes will not expand the variable
+# work=DevOps
+# post=Engineer
+# echo "I am a ${work} ${post} "
+
 # Ststem defined variables
+# this variables are defined by the system and we can use them
+# HOME, PATH, PWD, SHELL, USER, LOGNAME, HOSTNAME, UID, GROUPS, etc
+echo "My home directory is ${HOME}"
+echo "My current working directory is ${PWD}"
+echo "My shell is ${SHELL}"
+echo "My user is ${USER}"
+echo "My logname is ${LOGNAME}"
+echo "My hostname is ${HOSTNAME}"
+echo "My UID is ${UID}"
+echo "My groups are ${GROUPS}"
+echo "My path is ${PATH}"
 
-echo $name
-
-# using $VariableName
-echo  "My $name is Mayur and my age is $age"
-
-# Curly braces
-echo  "My ${name} is Mayur and my age is ${age}"
-
-work=DevOps
-post=Engineer
-echo "I am a ${work}${post} "
