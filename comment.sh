d72ec1536eef8d2047d36cab924fc275f4e4346d
mayur2905 <mayurgaikwad0101@gmail.com>
2024-05-27 00:39:01 +0530

    chore: Add multi-line comment to comment.sh

--- comment.sh
+++ comment.sh
@@ -5,3 +5,15 @@
 # This is a comment
 echo "Hello this is first shell script" # This is also a inline comment
 
+# Strong quotes
+echo ' my \
+name \
+is \
+Mayur\
+'
+
+echo "  my \
+name \
+is \
+Mayur\
+" # This is a multi-line comment
